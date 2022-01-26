824ca3097d37ea50a5950c334c3ea9705371052f
Neilsya29RPL <neilsyaams0902@gmail.com>
2022-01-26 21:35:06 +0700

    Add files via upload

--- online_store.sql
+++ online_store.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 22, 2021 at 10:31 AM
+-- Generation Time: Jan 26, 2022 at 03:33 PM
 -- Server version: 10.1.29-MariaDB
 -- PHP Version: 7.2.0
 
@@ -19,8 +19,32 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `toko_online`
+-- Database: `online_store`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_pembelian_produk`
+--
+
+CREATE TABLE `detail_pembelian_produk` (
+  `id_detail_pembelian_produk` int(11) NOT NULL,
+  `id_pembelian_produk` int(11) NOT NULL,
+  `id_produk` int(11) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `detail_pembelian_produk`
+--
+
+INSERT INTO `detail_pembelian_produk` (`id_detail_pembelian_produk`, `id_pembelian_produk`, `id_produk`, `qty`) VALUES
+(19, 15, 7, 1),
+(20, 16, 11, 2),
+(21, 17, 12, 3),
+(44, 35, 7, 1),
+(45, 35, 8, 1);
 
 -- --------------------------------------------------------
 
@@ -40,9 +64,31 @@ CREATE TABLE `pelanggan` (
 --
 
 INSERT INTO `pelanggan` (`id_pelanggan`, `nama`, `alamat`, `telp`) VALUES
-(1, 'Neilsya Amstrani', 'Tulungagung', '085806752070'),
-(2, 'Maulidatul Hijriyah', 'Malang', '082334556729'),
-(6, 'Neilsya Amstrani Maulidatul Hjriyah', 'Blitar', '081334789001');
+(1, 'Neilsya', 'Tulungagung', '085806752070'),
+(2, 'Maulidatul ', 'Malang', '082334556729'),
+(6, 'NeilsyaAMH', 'Blitar', '081334789001');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembelian_produk`
+--
+
+CREATE TABLE `pembelian_produk` (
+  `id_pembelian_produk` int(11) NOT NULL,
+  `id_petugas` int(11) NOT NULL,
+  `tanggal_beli` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pembelian_produk`
+--
+
+INSERT INTO `pembelian_produk` (`id_pembelian_produk`, `id_petugas`, `tanggal_beli`) VALUES
+(15, 8, '2021-10-21'),
+(16, 8, '2021-10-21'),
+(17, 8, '2021-10-21'),
+(35, 8, '2021-10-21');
 
 -- --------------------------------------------------------
 
@@ -63,9 +109,9 @@ CREATE TABLE `petugas` (
 --
 
 INSERT INTO `petugas` (`id_petugas`, `nama_petugas`, `username`, `password`, `level`) VALUES
-(1, 'Rini', 'PetugasRini', 'rini', 'Bawah'),
-(2, 'Santi', 'PetugasSanti', 'santi', 'Menengah'),
-(5, 'Melati', 'PetugasMelati', 'melati', 'Atas');
+(1, 'Rini', 'PetugasRini', 'b86872751de1e13c142d050acfd09842', 'Bawah'),
+(2, 'Santi', 'PetugasSanti', 'ae1d4b431ead52e5ee1788010e8ec110', 'Menengah'),
+(8, 'Neilsya Amstrani', 'PetugasNeilsya', '666b85b83cdf84cdc0d55820cb44bbc4', 'Atas');
 
 -- --------------------------------------------------------
 
@@ -86,20 +132,32 @@ CREATE TABLE `produk` (
 --
 
 INSERT INTO `produk` (`id_produk`, `nama_produk`, `deskripsi`, `harga`, `foto_produk`) VALUES
-(7, 'IPA', 'Buku IPA SMP/MTS KELAS VII', 75000, 'Buku_IPA1.jpg'),
-(8, 'IPS', 'Buku IPS SMP/MTS KELAS IX', 50000, 'Buku_IPS1.jpg'),
-(11, 'Seni Budaya', 'Buku SBK SMA/SMK KELAS XI	', 45000, 'Buku_SBK.jpg'),
-(12, 'Matematika', 'Buku MTK SMP/MTS KELAS VII', 79000, 'Buku_MTK.jpg');
+(7, 'Blouse Wanita', 'Ini Blouse Wanita', 75000, 'Baju1.jpg'),
+(8, 'Sweater Wanita', 'Ini Sweater Wanita', 55000, 'Baju4.jpg'),
+(11, 'Over All', 'Ini Over All', 95000, 'Baju2.jpg'),
+(12, 'Blouse Panjang', 'Ini Bouse', 115000, 'Baju5.jpg');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `detail_pembelian_produk`
+--
+ALTER TABLE `detail_pembelian_produk`
+  ADD PRIMARY KEY (`id_detail_pembelian_produk`);
+
+--
 -- Indexes for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
   ADD PRIMARY KEY (`id_pelanggan`);
+
+--
+-- Indexes for table `pembelian_produk`
+--
+ALTER TABLE `pembelian_produk`
+  ADD PRIMARY KEY (`id_pembelian_produk`);
 
 --
 -- Indexes for table `petugas`
@@ -118,16 +176,28 @@ ALTER TABLE `produk`
 --
 
 --
+-- AUTO_INCREMENT for table `detail_pembelian_produk`
+--
+ALTER TABLE `detail_pembelian_produk`
+  MODIFY `id_detail_pembelian_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+
+--
 -- AUTO_INCREMENT for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
   MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
+-- AUTO_INCREMENT for table `pembelian_produk`
+--
+ALTER TABLE `pembelian_produk`
+  MODIFY `id_pembelian_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
 -- AUTO_INCREMENT for table `petugas`
 --
 ALTER TABLE `petugas`
-  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `produk`
